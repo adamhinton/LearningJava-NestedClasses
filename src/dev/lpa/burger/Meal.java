97ce34f0d0513d4a29d7a81dfb3b37285d2a0ed8
adamd <adam.d.hinton@gmail.com>
2024-03-11 10:24:58 -0400

    Flesh out Item class

--- src/dev/lpa/burger/Meal.java
+++ src/dev/lpa/burger/Meal.java
@@ -1,4 +1,31 @@
 package dev.lpa.burger;
 
+// Three kinds of item: Burger, Drink, Side
+
 public class Meal {
+
+    private class Item{
+        private String name;
+        private String type;
+        private double price;
+
+        public Item(String name, String type) {
+            // Calls the three args constructor
+            this(name, type, 0);
+        }
+
+        public Item(String name, String type, double price) {
+            this.name = name;
+            this.type = type;
+            this.price = price;
+        }
+
+        @Override
+        public String toString() {
+            return "%10s%15s $%.2f".formatted(type, name, price)
+        }
+    }
+
+
+
 }
