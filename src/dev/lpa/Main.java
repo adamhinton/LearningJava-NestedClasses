77100d46b4a9453a0d369d37f7afd48339169b69
adamd <adam.d.hinton@gmail.com>
2024-03-11 10:12:08 -0400

    add static to Comparator

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -31,10 +31,15 @@ public class Main {
                 new StoreEmployee(10015, "Meg", 2019, "Target"),
                 new StoreEmployee(10515, "Joe", 2021, "Walmart"),
                 new StoreEmployee(10105, "Tom", 2020, "Macys"),
-                new StoreEmployee(10105, "Tom", 2020, "Macys"),
                 new StoreEmployee(10215, "Marty", 2018, "Walmart"),
                 new StoreEmployee(10322, "Bud", 2016, "Target")));
 
+        var comparator = new StoreEmployee.StoreComparator<>();
+        storeEmployees.sort(comparator);
+
+        for (StoreEmployee e : storeEmployees){
+            System.out.println(e);
+        }
 
     }
 }
--- src/dev/lpa/domain/StoreEmployee.java
+++ src/dev/lpa/domain/StoreEmployee.java
@@ -4,6 +4,19 @@ import java.util.Comparator;
 
 public class StoreEmployee extends Employee{
 
+    public static class StoreComparator<T extends StoreEmployee> implements Comparator<StoreEmployee> {
+        @Override
+        public int compare(StoreEmployee o1, StoreEmployee o2) {
+            int result = o1.store.compareTo(o2.store);
+
+            if (result == 0) {
+                return new Employee.EmployeeComparator<>("yearstarted").compare(o1, o2);
+            }
+
+            return result;
+        }
+    }
+
     private String store;
 
     public StoreEmployee() {
@@ -16,20 +29,8 @@ public class StoreEmployee extends Employee{
 
     @Override
     public String toString() {
-        return "%-8s$s".formatted(store, super.toString());
+        return "%-8s%s".formatted(store, super.toString());
     }
 
-    public class StoreComparator<T extends StoreEmployee> implements Comparator<StoreEmployee>{
-        @Override
-        public int compare(StoreEmployee o1, StoreEmployee o2) {
-            int result = o1.store.compareTo(o2.store);
-
-            if (result == 0) {
-                return new Employee.EmployeeComparator<>("yearstarted").compare(o1, o2);
-            }
-
-            return result;
-        }
-    }
 
 }
